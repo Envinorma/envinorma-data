56ee663b81e0ee6678155767c9cec5cc498690ba
remidbs <remi.delbouys@laposte.net>
2020-10-30 12:38:49 +0100

    added utils in LF API

--- src/lib/legifrance_API.py
+++ src/lib/legifrance_API.py
@@ -1,22 +1,25 @@
+import json
 import requests
-from typing import Dict
+from datetime import datetime
+from typing import Dict, Optional
 from requests_oauthlib import OAuth2Session
+from tqdm import tqdm
 
-_API_HOST = "https://sandbox-api.aife.economie.gouv.fr"
-_TOKEN_URL = 'https://sandbox-oauth.aife.economie.gouv.fr/api/oauth/token'
-_NOR_URL = f'{_API_HOST}/dila/legifrance-beta/lf-engine-app/consult/getJoWithNor'
+_API_HOST = 'https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app'
+_TOKEN_URL = 'https://oauth.aife.economie.gouv.fr/api/oauth/token'
+_NOR_URL = f'{_API_HOST}/consult/getJoWithNor'
 
 
 def get_legifrance_client() -> OAuth2Session:
     data = {
-        "grant_type": "client_credentials",
-        "client_id": '45a5cd6b-e9f3-445d-8a62-efa56b68c8ec',
-        "client_secret": CLIENT_SECRET,
-        "scope": "openid",
+        'grant_type': 'client_credentials',
+        'client_id': 'a5b0a4be-9406-4481-925d-e1bfb784f691',
+        'client_secret': CLIENT_SECRET,
+        'scope': 'openid',
     }
     response = requests.post(_TOKEN_URL, data=data)
     token = response.json()
-    client = OAuth2Session('45a5cd6b-e9f3-445d-8a62-efa56b68c8ec', token=token)
+    client = OAuth2Session('a5b0a4be-9406-4481-925d-e1bfb784f691', token=token)
     return client
 
 
@@ -30,3 +33,75 @@ def get_arrete_by_nor(nor: str, client: OAuth2Session) -> Dict:
     json_ = {'nor': nor}  # ex: nor = 'DEVP1706393A'
     response = client.post(_NOR_URL, json=json_)
     return _extract_response_content(response)
+
+
+def get_loda_list(nor: str, client: OAuth2Session) -> Dict:
+    url = _API_HOST + '/list/loda'
+    json_ = {'nor': nor}  # ex: nor = 'DEVP1706393A'
+    response = client.post(url, json=json_)
+    return _extract_response_content(response)
+
+
+def search(nor: str, client: OAuth2Session) -> Dict:
+    url = _API_HOST + '/search'
+    json_ = {
+        'fond': 'LODA_DATE',
+        'recherche': {
+            # 'champs': [
+            #     {
+            #         'criteres': [{'typeRecherche': 'EXACTE', 'valeur': f'{nor}', 'operateur': 'ET'}],
+            #         'operateur': 'ET',
+            #         'typeChamp': 'NOR',
+            #     }
+            # ],
+            'champs': [
+                {
+                    'typeChamp': 'NOR',
+                    'criteres': [{'typeRecherche': 'EXACTE', 'valeur': nor, 'operateur': 'ET'}],
+                    'operateur': 'ET',
+                }
+            ],
+            # 'filtres': [{'valeur': f'{nor}', 'facette': 'NOR'}],
+            'operateur': 'ET',
+            'pageNumber': 1,
+            'pageSize': 10,
+            'sort': 'CONFORME',
+            'typePagination': 'DEFAUT',
+        },
+    }
+    response = client.post(url, json=json_)
+    return _extract_response_content(response)
+
+
+def get_current_loda_via_cid(cid: str, client: OAuth2Session) -> Dict:
+    json_ = {'date': int(datetime.now().timestamp()) * 1000, 'textId': cid}
+    url = _API_HOST + '/consult/lawDecree'
+    response = client.post(url, json=json_)
+    return _extract_response_content(response)
+
+
+def get_article_by_id(cid: str, client: OAuth2Session) -> Dict:
+    json_ = {'id': cid}
+    url = _API_HOST + '/consult/getArticle'
+    response = client.post(url, json=json_)
+    return _extract_response_content(response)
+
+
+def download_text(cid: str, nor: str, client: Optional[OAuth2Session]) -> None:
+    if not client:
+        client = get_legifrance_client()
+    try:
+        res = get_current_loda_via_cid(cid, client)
+    except Exception as exc:
+        print(f'{cid}, {nor},', str(exc))
+        return
+    json.dump(res, open(f'data/AM/legifrance_texts/{nor}.json', 'w'), ensure_ascii=False)
+
+
+def download_all_lf_texts() -> None:
+    texts = json.load(open('data/AM/arretes_ministeriels.json'))
+    client = get_legifrance_client()
+    for text in tqdm(texts):
+        if 'nor' in text and 'cid' in text:
+            download_text(text['cid'], text['nor'], client)
+
