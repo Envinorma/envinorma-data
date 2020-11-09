bf54728dea283978e1af5e17a32929ca3643d966
remidbs <remi.delbouys@laposte.net>
2020-11-09 16:27:38 +0100

    added parametric AM generation

--- src/lib/parametric_am.py
+++ src/lib/parametric_am.py
@@ -1,8 +1,9 @@
 from copy import copy
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
+from datetime import datetime, timedelta
 from enum import Enum
 from typing import Any, Dict, KeysView, List, Optional, Set, Tuple, Union
-from lib.am_structure_extraction import ArreteMinisteriel, StructuredText
+from lib.am_structure_extraction import ArreteMinisteriel, StructuredText, load_structured_text
 
 
 class ParameterType(Enum):
@@ -10,14 +11,24 @@ class ParameterType(Enum):
     BOOLEAN = 'BOOLEAN'
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class Parameter:
     id: str
     type: ParameterType
 
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Parameter':
+        return Parameter(dict_['id'], ParameterType(dict_['type']))
+
 
 class ParameterEnum(Enum):
-    DATE_D_AUTORISATION = Parameter('date-d-autorisation', ParameterType.DATE)
+    DATE_AUTORISATION = Parameter('date-d-autorisation', ParameterType.DATE)
+    DATE_INSTALLATION = Parameter('date-d-installation', ParameterType.DATE)
 
 
 class ConditionType(Enum):
@@ -29,16 +40,63 @@ class ConditionType(Enum):
     LITTLER = 'LITTLER'
 
 
+def load_condition(dict_: Dict[str, Any]) -> 'Condition':
+    type_ = ConditionType(dict_['type'])
+    if type_ == ConditionType.AND:
+        return AndCondition.from_dict(dict_)
+    if type_ == ConditionType.OR:
+        return OrCondition.from_dict(dict_)
+    if type_ == ConditionType.EQUAL:
+        return Equal.from_dict(dict_)
+    if type_ == ConditionType.GREATER:
+        return Greater.from_dict(dict_)
+    if type_ == ConditionType.LITTLER:
+        return Littler.from_dict(dict_)
+    if type_ == ConditionType.RANGE:
+        return Range.from_dict(dict_)
+    raise ValueError(f'Unknown condition type {type_}')
+
+
 @dataclass
 class AndCondition:
     conditions: List['Condition']
     type: ConditionType = ConditionType.AND
+
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'AndCondition':
+        return AndCondition([load_condition(cd) for cd in dict_['conditions']])
 
 
 @dataclass
 class OrCondition:
     conditions: List['Condition']
     type: ConditionType = ConditionType.OR
+
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'OrCondition':
+        return OrCondition([load_condition(cd) for cd in dict_['conditions']])
+
+
+def parameter_value_to_dict(value: Any, type_: ParameterType) -> Any:
+    if type_ == ParameterType.DATE:
+        return int(value.timestamp())
+    return value
+
+
+def load_target(json_value: Any, type_: ParameterType) -> Any:
+    if type_ == ParameterType.DATE:
+        return datetime.fromtimestamp(json_value)
+    return json_value
 
 
 @dataclass
@@ -48,6 +106,18 @@ class Littler:
     strict: bool
     type: ConditionType = ConditionType.LITTLER
 
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        res['parameter'] = self.parameter.to_dict()
+        res['target'] = parameter_value_to_dict(self.target, self.parameter.type)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Littler':
+        parameter = Parameter.from_dict(dict_['parameter'])
+        return Littler(parameter, load_target(dict_['target'], parameter.type), dict_['strict'])
+
 
 @dataclass
 class Greater:
@@ -56,12 +126,36 @@ class Greater:
     strict: bool
     type: ConditionType = ConditionType.GREATER
 
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        res['parameter'] = self.parameter.to_dict()
+        res['target'] = parameter_value_to_dict(self.target, self.parameter.type)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Greater':
+        parameter = Parameter.from_dict(dict_['parameter'])
+        return Greater(parameter, load_target(dict_['target'], parameter.type), dict_['strict'])
+
 
 @dataclass
 class Equal:
     parameter: Parameter
     target: Any
     type: ConditionType = ConditionType.EQUAL
+
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        res['parameter'] = self.parameter.to_dict()
+        res['target'] = parameter_value_to_dict(self.target, self.parameter.type)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Equal':
+        parameter = Parameter.from_dict(dict_['parameter'])
+        return Equal(parameter, load_target(dict_['target'], parameter.type))
 
 
 @dataclass
@@ -73,6 +167,25 @@ class Range:
     right_strict: bool
     type: ConditionType = ConditionType.RANGE
 
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        res['type'] = self.type.value
+        res['parameter'] = self.parameter.to_dict()
+        res['left'] = parameter_value_to_dict(self.left, self.parameter.type)
+        res['right'] = parameter_value_to_dict(self.right, self.parameter.type)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Range':
+        parameter = Parameter.from_dict(dict_['parameter'])
+        return Range(
+            Parameter.from_dict(dict_['parameter']),
+            load_target(dict_['left'], parameter.type),
+            load_target(dict_['right'], parameter.type),
+            dict_['left_strict'],
+            dict_['right_strict'],
+        )
+
 
 Condition = Union[Equal, Range, Greater, Littler, AndCondition, OrCondition]
 
@@ -83,17 +196,40 @@ Ints = Tuple[int, ...]
 class SectionReference:
     path: Ints
 
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'SectionReference':
+        return SectionReference(tuple(dict_['path']))
+
 
 @dataclass
 class EntityReference:
     section: SectionReference
     outer_alinea_indices: Optional[List[int]]
 
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'EntityReference':
+        return EntityReference(SectionReference.from_dict(dict_['section']), dict_['outer_alinea_indices'])
+
 
 @dataclass
 class ConditionSource:
     explanation: str
     reference: EntityReference
+
+    def to_dict(self) -> Dict[str, Any]:
+        res = asdict(self)
+        return res
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'ConditionSource':
+        return ConditionSource(dict_['explanation'], EntityReference.from_dict(dict_['reference']))
 
 
 @dataclass
@@ -102,6 +238,21 @@ class ApplicationCondition:
     condition: Condition
     source: ConditionSource
 
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'targeted_entity': self.targeted_entity.to_dict(),
+            'condition': self.condition.to_dict(),
+            'source': self.source.to_dict(),
+        }
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'ApplicationCondition':
+        return ApplicationCondition(
+            EntityReference.from_dict(dict_['targeted_entity']),
+            load_condition(dict_['condition']),
+            ConditionSource.from_dict(dict_['source']),
+        )
+
 
 @dataclass
 class AlternativeSection:
@@ -109,6 +260,23 @@ class AlternativeSection:
     new_text: StructuredText
     condition: Condition
     source: ConditionSource
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'targeted_section': self.targeted_section.to_dict(),
+            'new_text': self.new_text.as_dict(),
+            'condition': self.condition.to_dict(),
+            'source': self.source.to_dict(),
+        }
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'AlternativeSection':
+        return AlternativeSection(
+            SectionReference.from_dict(dict_['targeted_section']),
+            load_structured_text(dict_['new_text']),
+            load_condition(dict_['condition']),
+            ConditionSource.from_dict(dict_['source']),
+        )
 
 
 @dataclass
@@ -132,6 +300,19 @@ class Parametrization:
             if path not in self.path_to_alternative_sections:
                 self.path_to_alternative_sections[path] = []
             self.path_to_alternative_sections[path].append(sec)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'application_conditions': [app.to_dict() for app in self.application_conditions],
+            'alternative_sections': [sec.to_dict() for sec in self.alternative_sections],
+        }
+
+    @classmethod
+    def from_dict(cls, dict_: Dict[str, Any]) -> 'Parametrization':
+        return Parametrization(
+            [ApplicationCondition.from_dict(app) for app in dict_['application_conditions']],
+            [AlternativeSection.from_dict(sec) for sec in dict_['alternative_sections']],
+        )
 
 
 def _generate_bool_condition(parameter_str: str) -> Equal:
@@ -165,6 +346,31 @@ def condition_to_str(condition: Condition) -> str:
         comp = '>' if condition.strict else '>='
         return f'Si {condition.parameter.id} {comp} {condition.target}:'
     raise NotImplementedError(f'stringifying condition {condition.type} is not implemented yet.')
+
+
+def _check_condition(condition: Condition) -> None:
+    if isinstance(condition, Equal):
+        assert condition.type == ConditionType.EQUAL
+    if isinstance(condition, Greater):
+        assert condition.type == ConditionType.GREATER
+    if isinstance(condition, Littler):
+        assert condition.type == ConditionType.LITTLER
+    if isinstance(condition, Range):
+        assert condition.type == ConditionType.RANGE
+    if isinstance(condition, AndCondition):
+        assert condition.type == ConditionType.AND
+    if isinstance(condition, OrCondition):
+        assert condition.type == ConditionType.OR
+    if isinstance(condition, (AndCondition, OrCondition)):
+        for subcondition in condition.conditions:
+            _check_condition(subcondition)
+
+
+def _check_parametrization(parametrization: Parametrization) -> None:
+    for app in parametrization.application_conditions:
+        _check_condition(app.condition)
+    for sec in parametrization.alternative_sections:
+        _check_condition(sec.condition)
 
 
 def _extract_parameters_from_condition(condition: Condition) -> List[Parameter]:
@@ -235,8 +441,13 @@ def _generate_reason_inactive(condition: Condition, parameter_values: Dict[Param
         return 'Aucune des conditions d\'application suivantes ne sont pas respectées: ' + ', '.join(str_)
     parameter = condition.parameter
     if isinstance(condition, Equal):
-        value = parameter_values[parameter]
-        return f'Le paramètre {parameter.id} n\'est pas égal à {value}'
+        return f'Le paramètre {parameter.id} n\'est pas égal à {condition.target}'
+    if isinstance(condition, Greater):
+        return f'Le paramètre {parameter.id} est inférieur à {condition.target}'
+    if isinstance(condition, Littler):
+        return f'Le paramètre {parameter.id} est supérieur à {condition.target}'
+    if isinstance(condition, Range):
+        return f'Le paramètre {parameter.id} n\'est pas entre {condition.left} et {condition.right}'
     raise NotImplementedError(f'stringifying condition {condition.type} is not implemented yet.')
 
 
@@ -267,3 +478,128 @@ def _apply_parameter_values_to_am(
         for i, section in enumerate(am.sections)
     ]
     return new_am
+
+
+def _extract_leaf_conditions(condition: Condition, parameter: Parameter) -> List[Condition]:
+    if isinstance(condition, (OrCondition, AndCondition)):
+        return [
+            cond for search_cond in condition.conditions for cond in _extract_leaf_conditions(search_cond, parameter)
+        ]
+    if condition.parameter.id == parameter.id:
+        return [condition]
+    return []
+
+
+def _extract_conditions_from_parametrization(parameter: Parameter, parametrization: Parametrization) -> List[Condition]:
+    return [
+        cond
+        for app in parametrization.application_conditions
+        for cond in _extract_leaf_conditions(app.condition, parameter)
+    ]
+
+
+def _generate_combinations(
+    options_dicts: List[Dict[str, Tuple[Parameter, Any]]]
+) -> Dict[Tuple[str, ...], Dict[Parameter, Any]]:
+    if len(options_dicts) == 0:
+        raise ValueError('Cannot combine zero options dicts')
+    if len(options_dicts) == 1:
+        return {(str_,): {param: target} for str_, (param, target) in options_dicts[0].items()}
+    rec_combinations = _generate_combinations(options_dicts[1:])
+    return {
+        name + (new_name,): {**combination, new_combination[0]: new_combination[1]}
+        for new_name, new_combination in options_dicts[0].items()
+        for name, combination in rec_combinations.items()
+    }
+
+
+def _change_value(value: Any) -> Any:
+    if isinstance(value, bool):
+        return not value
+    if isinstance(value, (float, int)):
+        return value + 1
+    if isinstance(value, datetime):
+        return value + timedelta(days=1)
+    raise NotImplementedError(f'Cannot generate changed value for type {type(value)}')
+
+
+def _check_condition_is_right_strict(condition: Condition) -> None:
+    if isinstance(condition, Range):
+        assert not condition.left_strict
+        assert condition.right_strict
+    elif isinstance(condition, Littler):
+        assert condition.strict
+    elif isinstance(condition, Greater):
+        assert not condition.strict
+    else:
+        raise ValueError(f'Unexpected type {type(condition)}')
+
+
+def _extract_sorted_targets(conditions: List[Condition], right_strict: bool) -> List[Any]:
+    targets: List[Any] = []
+    for condition in conditions:
+        if not isinstance(condition, (Littler, Greater, Range)):
+            raise ValueError(f'Excepting types (Littler, Greater, Range), received {type(condition)}')
+        if right_strict:
+            _check_condition_is_right_strict(condition)
+        if isinstance(condition, (Littler, Greater)):
+            targets.append(condition.target)
+        else:
+            targets.extend([condition.left, condition.right])
+    return list(sorted(list(set(targets))))
+
+
+def _mean(a: Any, b: Any):
+    if isinstance(a, datetime) and isinstance(b, datetime):
+        return datetime.fromtimestamp((a.timestamp() + b.timestamp()) / 2)
+    return (a + b) / 2
+
+
+def _extract_interval_midpoints(interval_sides: List[Any]) -> List[Any]:
+    left = (interval_sides[0] - timedelta(1)) if isinstance(interval_sides[0], datetime) else (interval_sides[0] - 1)
+    right = (
+        (interval_sides[-1] + timedelta(1)) if isinstance(interval_sides[-1], datetime) else (interval_sides[-1] + 1)
+    )
+    midpoints = [_mean(a, b) for a, b in zip(interval_sides[1:], interval_sides[:-1])]
+    return [left] + midpoints + [right]
+
+
+def _generate_options_dict(conditions: List[Condition]) -> Dict[str, Tuple[Parameter, Any]]:
+    types = {condition.type for condition in conditions}
+    if types == {ConditionType.EQUAL}:
+        condition = conditions[0]
+        assert isinstance(condition, Equal)
+        targets = list({cd.target for cd in conditions if isinstance(cd, Equal)})
+        if len(targets) == 1:
+            return {
+                f'{condition.parameter.id} == {condition.target}': (condition.parameter, condition.target),
+                f'{condition.parameter.id} != {condition.target}': (
+                    condition.parameter,
+                    _change_value(condition.target),
+                ),
+            }
+    if ConditionType.EQUAL not in types:
+        targets = _extract_sorted_targets(conditions, True)
+        values = _extract_interval_midpoints(targets)
+        condition = conditions[0]
+        assert isinstance(condition, (Range, Greater, Littler))
+        parameter = condition.parameter
+        param_names = [f'{parameter.id} < {target}' for target in targets] + [f'{parameter.id} >= {targets[-1]}']
+        return {param_name: (parameter, value) for param_name, value in zip(param_names, values)}
+    raise NotImplementedError(f'Option dict generation not implemented for {conditions}')
+
+
+def _generate_all_am_versions(
+    am: ArreteMinisteriel, parametrization: Parametrization
+) -> Dict[Tuple[str, ...], ArreteMinisteriel]:
+    parameters = _extract_parameters_from_parametrization(parametrization)
+    options_dicts: List[Dict[str, Tuple[Parameter, Any]]] = []
+    for parameter in parameters:
+        conditions = _extract_conditions_from_parametrization(parameter, parametrization)
+        options_dicts.append(_generate_options_dict(conditions))
+
+    combinations = _generate_combinations(options_dicts)
+    return {
+        combination_name: _apply_parameter_values_to_am(am, parametrization, parameter_values)
+        for combination_name, parameter_values in combinations.items()
+    }
---
+++ src/tests/test_parametric_am.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+from lib.parametric_am import (
+    Equal,
+    Greater,
+    Littler,
+    ParameterType,
+    Range,
+    _mean,
+    _extract_interval_midpoints,
+    _generate_options_dict,
+    _extract_sorted_targets,
+    _generate_combinations,
+    _change_value,
+    Condition,
+    Parameter,
+)
+
+
+def test_mean():
+    assert _mean(0, 2) == 1
+    assert _mean(datetime(2020, 1, 1), datetime(2020, 1, 3)) == datetime(2020, 1, 2)
+
+
+def test_extract_interval_midpoints():
+    assert _extract_interval_midpoints([0, 1, 2, 3]) == [-1, 0.5, 1.5, 2.5, 4]
+    res = [datetime(2019, 12, 31), datetime(2020, 1, 2), datetime(2020, 1, 4)]
+    assert _extract_interval_midpoints([datetime(2020, 1, 1), datetime(2020, 1, 3)]) == res
+
+
+def test_generate_options_dict():
+    parameter = Parameter('test', ParameterType.BOOLEAN)
+    conditions = [Equal(parameter, True), Equal(parameter, True)]
+    res = _generate_options_dict(conditions)
+    assert len(res) == 2
+    assert 'test == True' in res
+    assert 'test != True' in res
+    assert res['test == True'] == (parameter, True)
+    assert res['test != True'] == (parameter, False)
+
+
+def test_extract_sorted_targets():
+    parameter = Parameter('test', ParameterType.DATE)
+    conditions = [
+        Range(parameter, datetime(2020, 1, 1), datetime(2021, 1, 1), False, True),
+        Littler(parameter, datetime(2020, 1, 1), True),
+        Greater(parameter, datetime(2021, 1, 1), False),
+    ]
+    assert _extract_sorted_targets(conditions, True) == [datetime(2020, 1, 1), datetime(2021, 1, 1)]
+
+
+def test_generate_options_dict_2():
+    parameter = Parameter('test', ParameterType.DATE)
+    conditions = [
+        Range(parameter, datetime(2020, 1, 1), datetime(2021, 1, 1), False, True),
+        Littler(parameter, datetime(2020, 1, 1), True),
+        Greater(parameter, datetime(2021, 1, 1), False),
+    ]
+    res = _generate_options_dict(conditions)
+    assert len(res) == 3
+    str_dt_20 = '2020-01-01 00:00:00'
+    str_dt_21 = '2021-01-01 00:00:00'
+    assert f'test < {str_dt_20}' in res
+    assert f'test < {str_dt_21}' in res
+    assert f'test >= {str_dt_21}' in res
+    assert res[f'test < {str_dt_20}'] == (parameter, datetime(2019, 12, 31))
+    assert res[f'test < {str_dt_21}'] == (parameter, datetime(2020, 7, 2, 1, 0))
+    assert res[f'test >= {str_dt_21}'] == (parameter, datetime(2021, 1, 2))
+
+
+def test_change_value():
+    assert not _change_value(True)
+    assert _change_value(False)
+    assert _change_value(1) == 2
+    assert _change_value(2.0) == 3
+    assert _change_value(datetime(2020, 1, 1)) == datetime(2020, 1, 2)
+
+
+def test_generate_combinations():
+    parameter_1 = Parameter('test_1', ParameterType.DATE)
+    parameter_2 = Parameter('test_2', ParameterType.BOOLEAN)
+    option_dict_1 = {
+        'test_1 < a': (parameter_1, datetime(2021, 1, 1)),
+        'test_1 >= a': (parameter_1, datetime(2022, 1, 1)),
+    }
+    option_dict_2 = {'test_2 == True': (parameter_2, True), 'test_2 != True': (parameter_2, False)}
+    res = _generate_combinations([option_dict_1, option_dict_2])
+    assert len(res) == 4
+    assert ('test_2 == True', 'test_1 < a') in res
+    assert ('test_2 != True', 'test_1 < a') in res
+    assert ('test_2 == True', 'test_1 >= a') in res
+    assert ('test_2 != True', 'test_1 >= a') in res
