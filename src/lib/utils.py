055750415ee942bc0b62b7730ed14e0ca9f7b7de
remidbs <remi.delbouys@laposte.net>
2020-12-15 15:27:52 +0100

    improve json writing

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -3,12 +3,13 @@ import traceback
 from typing import Union, List, Dict
 
 
-def write_json(obj: Union[Dict, List], filename: str, safe: bool = False) -> None:
+def write_json(obj: Union[Dict, List], filename: str, safe: bool = False, pretty: bool = True) -> None:
+    indent = 4 if pretty else None
     with open(filename, 'w') as file_:
         if not safe:
-            json.dump(obj, file_, indent=4, sort_keys=True, ensure_ascii=False)
+            json.dump(obj, file_, indent=indent, sort_keys=True, ensure_ascii=False)
         else:
             try:
-                json.dump(obj, file_, indent=4, sort_keys=True, ensure_ascii=False)
+                json.dump(obj, file_, indent=indent, sort_keys=True, ensure_ascii=False)
             except Exception:  # pylint: disable=broad-except
                 print(traceback.format_exc())
