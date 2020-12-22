8ed8bc1e8d80ee2a509003f07d3bd9e1c181ce7b
remidbs <remi.delbouys@laposte.net>
2020-12-22 17:58:54 +0100

    added tests for structure edition

---
+++ src/back_office/tests/test_structure_edition.py
@@ -0,0 +1,44 @@
+from lib.data import Table
+from lib.structure_extraction import Title
+import dash_html_components as html
+from back_office.structure_edition import (
+    _get_html_heading_classname,
+    _extract_dropdown_values,
+    _modify_elements_with_new_title_levels,
+    _ensure_title,
+)
+
+
+def test_get_html_heading_classname():
+    assert isinstance(_get_html_heading_classname(1)(), html.H1)
+    assert isinstance(_get_html_heading_classname(6)(), html.H6)
+    assert isinstance(_get_html_heading_classname(7)(), html.H6)
+
+
+def test_extract_dropdown_values():
+    assert _extract_dropdown_values([]) == []
+    assert _extract_dropdown_values([{'type': 'Dropdown', 'props': {'value': 1}}]) == [1]
+    assert _extract_dropdown_values(
+        [{'type': 'Dropdown', 'props': {'value': 1}}, {'type': 'Dropdown', 'props': {'value': 2}}]
+    ) == [1, 2]
+    assert _extract_dropdown_values(
+        [{'type': 'Dropdown', 'props': {'value': 1}}, {'type': 'xx', 'props': {'children': []}}]
+    ) == [1]
+    dicts = [
+        {'type': 'Dropdown', 'props': {'value': 1}},
+        {'type': 'xx', 'props': {'children': {'type': 'Dropdown', 'props': {'value': 2}}}},
+    ]
+    assert _extract_dropdown_values(dicts) == [1, 2]
+
+
+def test_modify_elements_with_new_title_levels():
+    assert _modify_elements_with_new_title_levels([], []) == []
+    assert _modify_elements_with_new_title_levels([Title('', 0)], [None]) == ['']
+    assert _ensure_title(_modify_elements_with_new_title_levels([Title('', 0)], [1])[0]).level == 1
+    assert _ensure_title(_modify_elements_with_new_title_levels([Title('', 0)], [1])[0]).text == ''
+    assert _modify_elements_with_new_title_levels([''], [None]) == ['']
+    assert _ensure_title(_modify_elements_with_new_title_levels([''], [1])[0]).level == 1
+    assert _ensure_title(_modify_elements_with_new_title_levels([''], [1])[0]).text == ''
+    assert _modify_elements_with_new_title_levels(['', ''], [None, None]) == ['', '']
+    tb = Table([])
+    assert _modify_elements_with_new_title_levels(['', '', tb], [None, None, None]) == ['', '', tb]
