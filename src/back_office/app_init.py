d59f349d774b1de479b37cb084cf86dfd733515f
remidbs <remi.delbouys@laposte.net>
2021-02-04 14:49:36 +0100

    changed app name

--- src/back_office/app_init.py
+++ src/back_office/app_init.py
@@ -5,5 +5,5 @@ app = dash.Dash(
     __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP, __file__.replace('app_init.py', 'assets/style.css')],
     suppress_callback_exceptions=True,
-    title='Envinorma',
+    title='Back office - Envinorma',
 )
