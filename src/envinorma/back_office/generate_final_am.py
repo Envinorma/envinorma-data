88e971bcff78b7a7e2b5b1a4b3b89c235fb57003
remidbs <remi.delbouys@laposte.net>
2021-02-19 11:31:47 +0100

    improved AM generation and tests

--- src/envinorma/back_office/generate_final_am.py
+++ src/envinorma/back_office/generate_final_am.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
+from datetime import datetime
 from typing import Dict, Optional, Tuple
 
 from envinorma.am_enriching import (
@@ -9,13 +10,68 @@ from envinorma.am_enriching import (
     remove_null_applicabilities,
 )
 from envinorma.back_office.fetch_data import load_initial_am, load_parametrization, load_structured_am
-from envinorma.data import AMMetadata, ArreteMinisteriel, add_metadata
-from envinorma.data_build.manual_enrichments import get_manual_combinations, get_manual_post_process
-from envinorma.parametrization import Parametrization
+from envinorma.data import AMMetadata, ArreteMinisteriel, ClassementWithAlineas, Regime, add_metadata
+
+from envinorma.parametrization import Combinations, Parametrization
+from envinorma.parametrization.conditions import ParameterEnum
 from envinorma.parametrization.parametric_am import generate_all_am_versions
 from envinorma.topics.topics import TOPIC_ONTOLOGY
 
 AMVersions = Dict[Tuple[str, ...], ArreteMinisteriel]
+
+_1510_IDS = ('DEVP1706393A', 'JORFTEXT000034429274')
+
+
+def _manual_1510_post_process(am: ArreteMinisteriel, regime: str) -> ArreteMinisteriel:
+    classements = am.classements
+    new_classements = [cl for cl in classements if cl.regime.value == regime]
+    if len(new_classements) != 1:
+        raise ValueError(new_classements)
+    classement = new_classements[0]
+    new_classements_with_alineas = [ClassementWithAlineas(classement.rubrique, classement.regime, [])]
+    new_id = f'{am.id}_{regime}'  # to avoid having duplicate ids
+    return replace(am, classements=new_classements, classements_with_alineas=new_classements_with_alineas, id=new_id)
+
+
+def _extract_regime(am_id: str, name: Tuple[str, ...]) -> Optional[str]:
+    if am_id in _1510_IDS:  # Hack for this very special AM
+        assert name[0][:4] == 'reg_'
+        return name[0][4]
+    return None
+
+
+def _post_process(am_id: str, am: ArreteMinisteriel, regime_str: Optional[str]) -> ArreteMinisteriel:
+    if am_id in _1510_IDS:  # Hack for this very special AM
+        if regime_str is None:
+            raise ValueError('Cannot add 1510 metadata: need regime but got None.')
+        am = _manual_1510_post_process(am, regime_str)
+    return add_summary(am)
+
+
+def _generate_1510_combinations() -> Combinations:
+    regime = ParameterEnum.REGIME.value
+    date = ParameterEnum.DATE_INSTALLATION.value
+    return {
+        ('reg_A_no_date',): {regime: Regime.A},
+        ('reg_A', 'date_before_2003'): {regime: Regime.A, date: datetime(2000, 1, 1)},
+        ('reg_A', 'date_between_2003_and_2017'): {regime: Regime.A, date: datetime(2010, 1, 1)},
+        ('reg_A', 'date_after_2017'): {regime: Regime.A, date: datetime(2020, 1, 1)},
+        ('reg_E_no_date',): {regime: Regime.E},
+        ('reg_E', 'date_before_2003'): {regime: Regime.E, date: datetime(2000, 1, 1)},
+        ('reg_E', 'date_between_2003_and_2010'): {regime: Regime.E, date: datetime(2006, 1, 1)},
+        ('reg_E', 'date_between_2010_and_2017'): {regime: Regime.E, date: datetime(2015, 1, 1)},
+        ('reg_E', 'date_after_2017'): {regime: Regime.E, date: datetime(2020, 1, 1)},
+        ('reg_D_no_date',): {regime: Regime.D},
+        ('reg_D', 'date_before_2009'): {regime: Regime.D, date: datetime(2000, 1, 1)},
+        ('reg_D', 'date_between_2009_and_2017'): {regime: Regime.D, date: datetime(2010, 1, 1)},
+        ('reg_D', 'date_after_2017'): {regime: Regime.D, date: datetime(2020, 1, 1)},
+    }
+
+
+def _get_manual_combinations(am_id: str) -> Optional[Combinations]:
+    if am_id in _1510_IDS:
+        return _generate_1510_combinations()
+    return None
 
 
 def apply_parametrization(
@@ -24,20 +80,22 @@ def apply_parametrization(
     if not am:
         return None
     enriched_am = remove_null_applicabilities(am)
-    manual_combinations = get_manual_combinations(am_id)  # For AM 1510 mainly, none otherwise
+    manual_combinations = _get_manual_combinations(am_id)  # For AM 1510 mainly, none otherwise
     all_versions = generate_all_am_versions(enriched_am, parametrization, True, manual_combinations)
-    return {name: get_manual_post_process(am_id)(add_summary(am_), name) for name, am_ in all_versions.items()}
+    return {name: _post_process(am_id, am_, _extract_regime(am_id, name)) for name, am_ in all_versions.items()}
 
 
 def _add_enrichments(am: ArreteMinisteriel, metadata: AMMetadata) -> ArreteMinisteriel:
-    return remove_null_applicabilities(
-        add_table_inspection_sheet_data(
-            detect_and_add_topics(add_references(add_metadata(am, metadata)), TOPIC_ONTOLOGY)
+    return add_summary(
+        remove_null_applicabilities(
+            add_table_inspection_sheet_data(
+                detect_and_add_topics(add_references(add_metadata(am, metadata)), TOPIC_ONTOLOGY)
+            )
         )
     )
 
 
-def _enrich_am(am: Optional[ArreteMinisteriel], metadata: AMMetadata) -> Optional[ArreteMinisteriel]:
+def enrich_am(am: Optional[ArreteMinisteriel], metadata: AMMetadata) -> Optional[ArreteMinisteriel]:
     if not am:
         return None
     return _add_enrichments(am, metadata)
@@ -54,7 +112,7 @@ def generate_final_am(metadata: AMMetadata) -> FinalAM:
     am = load_structured_am(cid) or load_initial_am(cid)
     if not am:
         raise ValueError('Expecting one AM to proceed.')
-    am = _enrich_am(am, metadata)
+    am = enrich_am(am, metadata)
     parametrization = load_parametrization(cid) or Parametrization([], [])
     am_versions = apply_parametrization(cid, am, parametrization)
     return FinalAM(base_am=am, am_versions=am_versions)
--- src/scripts/generate_seeds.py
+++ src/scripts/generate_seeds.py
@@ -1,12 +1,9 @@
 '''
 Download last versions of AM and send them to envinorma-web
 '''
-from typing import Dict
-from envinorma.config import generate_parametric_descriptor
-from envinorma.back_office.generate_final_am import AMVersions, apply_parametrization
-from envinorma.parametrization import Parametrization
-from envinorma.data import ArreteMinisteriel, add_metadata
+import json
 import os
+from typing import Any, Dict, List
 
 from envinorma.back_office.fetch_data import (
     load_all_am_statuses,
@@ -14,13 +11,19 @@ from envinorma.back_office.fetch_data import (
     load_all_parametrizations,
     load_all_structured_am,
 )
+from envinorma.back_office.generate_final_am import AMVersions, apply_parametrization, enrich_am
 from envinorma.back_office.utils import ID_TO_AM_MD, AMStatus
+from envinorma.config import generate_parametric_descriptor
+from envinorma.data import ArreteMinisteriel
+from envinorma.parametrization import Parametrization
 from envinorma.utils import ensure_not_none, write_json
 from tqdm import tqdm
 
 _OUTPUT_FOLDER = 'seeds'
 _OUTPUT_FOLDER = '/Users/remidelbouys/EnviNorma/envinorma-web/db/seeds'
 _ENRICHED_OUTPUT_FOLDER = os.path.join(_OUTPUT_FOLDER, 'enriched_arretes')
+_AM_LIST = os.path.join(_OUTPUT_FOLDER, 'am_list.json')
+_1510_IDS = ('DEVP1706393A', 'JORFTEXT000034429274')
 
 
 def _dump_am_versions(am_id: str, versions: AMVersions) -> None:
@@ -37,25 +40,85 @@ def _copy_enriched_am(id_: str, am: ArreteMinisteriel, parametrization: Parametr
 
 
 def _load_id_to_text() -> Dict[str, ArreteMinisteriel]:
+    print('loading texts.')
     structured_texts = load_all_structured_am()
     id_to_structured_text = {text.id: text for text in structured_texts}
     initial_texts = load_all_initial_am()
     id_to_initial_text = {text.id: text for text in initial_texts}
     return {
-        id_: add_metadata(ensure_not_none(id_to_structured_text.get(id_) or id_to_initial_text.get(id_)), md)
-        for id_, md in ID_TO_AM_MD.items()
+        id_: ensure_not_none(
+            enrich_am(ensure_not_none(id_to_structured_text.get(id_) or id_to_initial_text.get(id_)), md)
+        )
+        for id_, md in tqdm(ID_TO_AM_MD.items())
     }
+
+
+def _load_1510_am_no_date() -> List[Dict[str, Any]]:
+    return [
+        json.load(open(os.path.join(_ENRICHED_OUTPUT_FOLDER, f'JORFTEXT000034429274_reg_{regime_str}_no_date.json')))
+        for regime_str in ['A', 'E', 'D']
+    ]
+
+
+def print_input_id(func):
+    def _func(am: ArreteMinisteriel):
+        try:
+            func(am)
+        except:
+            print(am.id)
+            raise
+
+    return _func
+
+
+@print_input_id
+def _check_am(am: ArreteMinisteriel) -> None:
+    if am.id in _1510_IDS:
+        raise ValueError('1510 should not be in AM list as such')
+    regimes = {clas.regime for clas in am.classements}
+    if len(regimes) != 1:
+        raise ValueError(regimes)
+    assert am.summary is not None
+    assert am.legifrance_url is not None
+    assert am.aida_url is not None
+
+
+def _load_am_list() -> List[ArreteMinisteriel]:
+    return [ArreteMinisteriel.from_dict(x) for x in json.load(open(_AM_LIST))]
+
+
+def _load_enriched_am_list() -> List[ArreteMinisteriel]:
+    return [
+        ArreteMinisteriel.from_dict(json.load(open(os.path.join(_ENRICHED_OUTPUT_FOLDER, file_))))
+        for file_ in os.listdir(_ENRICHED_OUTPUT_FOLDER)
+    ]
+
+
+def _check_seeds() -> None:
+    am_list = _load_am_list()
+    for am in am_list:
+        _check_am(am)
+    all_ids = {am.id for am in am_list}
+    assert 'JORFTEXT000034429274_A' in all_ids
+    assert 'JORFTEXT000034429274_E' in all_ids
+    assert 'JORFTEXT000034429274_D' in all_ids
+    enriched_am = _load_enriched_am_list()
+    for am in enriched_am:
+        _check_am(am)
 
 
 def run():
     parametrizations = load_all_parametrizations()
     statuses = load_all_am_statuses()
     id_to_am = _load_id_to_text()
-    all_ams = [am.to_dict() for am in id_to_am.values()]
-    write_json(all_ams, os.path.join(_OUTPUT_FOLDER, 'am_list.json'), pretty=False)
+    all_ams = [am.to_dict() for am_id, am in id_to_am.items() if am_id not in _1510_IDS]
     for id_ in tqdm(ID_TO_AM_MD):
         if statuses[id_] == AMStatus.VALIDATED:
             _copy_enriched_am(id_, id_to_am[id_], parametrizations[id_])
+            if id_ in _1510_IDS:
+                all_ams.extend(_load_1510_am_no_date())
+    write_json(all_ams, _AM_LIST, pretty=False)
+    _check_seeds()
 
 
 if __name__ == '__main__':
